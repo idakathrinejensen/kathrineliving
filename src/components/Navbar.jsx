32f4723ab316099bbda30508dde58929e349c49a
FrederikSolbo <frederik.solbo@me.com>
2026-08-16 21:48:26 +0200

    Language toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import logo from '../assets/logo/KathrineLiving_logo_pos.png'
 
 function Navbar () {
     const [scrolled, setScrolled] = useState(false)
+    const [isSelected, setSelected] = useState(false)
 
     useEffect(() => {
         const handleScroll = () => {
@@ -28,6 +29,11 @@ function Navbar () {
                 <Link to="/hvordan-og-pris">Hvordan og pris</Link>
                 <Link to="/det-siger-kunderne">Det siger kunderne</Link>
                 <Link to="/om">Om</Link>
+                <div className='language-toggle'>
+                    <button className='language-button'>DA</button>
+                    <div className='divider'>/</div>
+                    <button className='language-button'>EN</button>
+                </div>
                 <Link to="/kontakt" className="nav-contact">KONTAKT</Link>
             </div>
         </nav>
