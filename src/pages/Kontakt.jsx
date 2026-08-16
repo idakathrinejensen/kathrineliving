ab2dea8d15c4f09677f593a33dcac54e61a10ef6
FrederikSolbo <frederik.solbo@me.com>
2026-08-16 20:57:11 +0200

    Contact form

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,54 +1,109 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 import { useForm, ValidationError } from '@formspree/react';
+import './Kontakt.css'
 
 function Kontakt () {
 
     const [state, handleSubmit] = useForm("mbgrkbrw");
     if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
+        return <div className='contact-page'><p className='form-completed'>Thanks for joining!</p></div>;
     }
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [usernameExists, setUsernameExists] = useState(false);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [isFilled, setIsFilled] = useState({
+        name: false,
+        email: false,
+        message: false,
+    });
+
+    const [isValidated, setIsValidated] = useState({
+        name: true,
+        email: true,
+        message: true,
+    });
+
+    function handleFilled(e) {
+        const newValue = e.target.value;
+        const fieldName = e.target.name;
+
+        setIsFilled((prev) => ({
+        ...prev,
+        [fieldName]: newValue.length > 0,
+        }));
+
+        setIsValidated((prev) => ({
+        ...prev,
+        [fieldName]: newValue.length > 0,
+        }));
+    }
+
+    useEffect(() => {
+        const allFilled =
+        isFilled.name &&
+        isFilled.email &&
+        isFilled.message;
+
+        setIsButtonDisabled(!allFilled);
+    }, [isFilled, usernameExists]);
+
     return (
-        <div>
-            <h1>Kontakt</h1>
-            <div>hi</div>
-            <div>hi</div>
-            <div>hi</div>
-            <div>hi</div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">
-                    Name
-                </label>
-                <input
-                    id="name"
-                    name="name"
-                />
-                <label htmlFor="email">
-                    Email Address
-                </label>
-                <input
-                    id="email"
-                    type="email" 
-                    name="email"
-                />
-                <ValidationError 
-                    prefix="Email" 
-                    field="email"
-                    errors={state.errors}
-                />
-                <textarea
-                    id="message"
-                    name="message"
-                />
-                <ValidationError 
-                    prefix="Message" 
-                    field="message"
-                    errors={state.errors}
-                />
-                <button type="submit" disabled={state.submitting}>
-                    Submit
-                </button>
-            </form>
+        <div className='contact-page'>
+            <div className='contact-form'>
+                <h1>Kontakt</h1>
+                <form onSubmit={handleSubmit} className='form-items'>
+                    <label htmlFor="name" className='form-item label'>
+                        Name
+                    </label>
+                    <input
+                        id="name"
+                        name="name"
+                        className='form-item input'
+                        onInput={handleFilled}
+                        onBlur={handleFilled}
+                        autoFocus
+                    />
+                    <label htmlFor="email" className='form-item label'>
+                        Email Address
+                    </label>
+                    <input
+                        id="email"
+                        type="email" 
+                        name="email"
+                        className='form-item input'
+                        onInput={handleFilled}
+                        onBlur={handleFilled}
+                    />
+                    <ValidationError 
+                        prefix="Email" 
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <label htmlFor="message" className='form-item label'>
+                        Message
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        className='form-item input message'
+                        onInput={handleFilled}
+                        onBlur={handleFilled}
+                    />
+                    <ValidationError 
+                        prefix="Message" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button
+                        type="submit"
+                        className={isButtonDisabled ? "submit-button-disabled" : "submit-button-enabled"}
+                        disabled={isButtonDisabled || isLoading}
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
